a6af0fd7d508ca4031a128d38d3cd77390de431b
codercai <545347837@qq.com>
2021-01-24 16:25:59 +0800

    Update diy-part2.sh
    fixed mt76

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,3 +13,9 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's|pcdata(boardinfo.system or "?")|luci.sys.exec("uname -m") or "?"|g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
+# Fix mt76 wireless driver
+pushd package/kernel/mt76
+rm -f Makefile
+wget https://raw.githubusercontent.com/openwrt/openwrt/master/package/kernel/mt76/Makefile
+sed -i '/mt7662u_rom_patch.bin/a\\techo mt76-usb disable_usb_sg=1 > $\(1\)\/etc\/modules.d\/mt76-usb' Makefile
+popd
