c4e3a1f31091f362969a3ad6c5f0ec4aac7524f4
codercai <545347837@qq.com>
2021-01-24 16:22:23 +0800

    Update diy-part2.sh
    修复核心显示

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,4 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's|pcdata(boardinfo.system or "?")|luci.sys.exec("uname -m") or "?"|g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
